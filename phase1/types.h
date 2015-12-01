fb4279192539e1175e40678de288d866337f7d61
Enrico Martelli <mrcnrt95e30@gmail.com>
2015-12-01 11:03:52 +0100

    Use of in syntax for list parameters

--- phase1/types.h
+++ phase1/types.h
@@ -8,13 +8,10 @@ struct pcb_t {
     struct pcb_t *p_parent; /* pointer to parent */
     struct semd_t *p_cursem; /* pointer to the semd_t on which process blocked */
     state_t p_s; /* processor state */
-    struct clist p_list; /* process list */
-    struct clist p_children; /* children list entry point*/
-    struct clist p_siblings; /* children list: links to the siblings */
+    in(p_list, p_children, p_siblings);
 };
 
 struct semd_t {
     int *s_semAdd; /* pointer to the semaphore */
-    struct clist s_link; /* ASL linked list */
-    struct clist s_procq; /* blocked process queue */
+    in(s_link, s_procq);
 };
