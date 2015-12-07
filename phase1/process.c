8cb0c847ad83ee5a984925ab318d196c5a6be0bc
Enrico Martelli <mrcnrt95e30@gmail.com>
2015-12-07 18:24:47 +0100

    First draft for process.c

--- phase1/process.c
+++ phase1/process.c
@@ -3,13 +3,30 @@
 void initPcbs() {
     static struct pcb_t pcbTable[MAXPROC];
 
-    // @TODO: implement initPcbs
+    for (int i = 0; i < MAXPROC; i++) {
+        clist_enqueue(&pcbTable[i], &pcbFree, p_list);
+    }
 }
 
 struct pcb_t *allocPcb() {
-    // @TODO: implement allocPcb
+    if (clist_empty(pcbFree)) {
+        return NULL;
+    }
+
+    struct pcb_t *elem;
+    elem = clist_head(elem, pcbFree, p_list);
+    clist_dequeue(&pcbFree);
+
+    elem->p_parent = NULL;
+    elem->p_cursem = NULL;
+    elem->p_s = NULL;
+    elem->p_list = CLIST_INIT;
+    elem->p_children = CLIST_INIT;
+    elem->p_siblings = CLIST_INIT;
+
+    return elem;
 }
 
 void freePcb(struct pcb_t *p) {
-    // @TODO: implement freePcb
+    clist_enqueue(p, &pcbFree, p_list);
 }
