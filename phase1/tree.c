093e780c64f445a8199fa0d11b1b43b020e0fd5d
Enrico Martelli <mrcnrt95e30@gmail.com>
2015-12-14 11:43:01 +0100

    Added first draft for tree.c

--- phase1/tree.c
+++ phase1/tree.c
@@ -1,17 +1,40 @@
 #include "tree.h"
 
 int emptyChild(struct pcb_t *p) {
-    // @TODO: implement emptyChild
+    return clist_empty(p->p_children) ? TRUE : FALSE;
 }
 
 void insertChild(struct pcb_t *parent, struct pcb_t *p) {
-    // @TODO: implement insertChild
+    p->p_parent = parent;
 }
 
 struct pcb_t *removeChild(struct pcb_t *p) {
-    // @TODO: implement removeChild
+    if (clist_empty(p->p_children)) {
+        return NULL;
+    }
+
+    struct pcb_t *elem;
+    elem = clist_head(elem, p->p_children, p_siblings);
+    clist_dequeue(q);
+
+    elem->p_parent = NULL;
+
+    return elem;
 }
 
 struct pcb_t *outChild(struct pcb_t *p) {
-    // @TODO: implement outChild
+    struct pcb_t *scan;
+    void *tmp = NULL;
+
+    clist_foreach(scan, p->p_parent, p_siblings, tmp) {
+        if (p == scan) {
+            clist_foreach_delete(scan, p->p_parent, p_siblings, tmp);
+
+            p->p_parent = NULL;
+
+            return p;
+        }
+    }
+
+    return NULL;
 }
