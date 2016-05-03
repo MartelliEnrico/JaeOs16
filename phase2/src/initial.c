8bdee56ecd34f3a1d450303b8ca0a73bd81afe2d
Enrico Martelli <mrcnrt95e30@gmail.com>
2016-05-03 18:30:13 +0200

    Added instantiation of test process

--- phase2/src/initial.c
+++ phase2/src/initial.c
@@ -3,6 +3,7 @@
 
 #include "const.h"
 #include "clist.h"
+#include "types.h"
 #include "pcb.h"
 #include "asl.h"
 
@@ -32,6 +33,22 @@ void initSemaphores() {
     }
 }
 
+void test() {
+    // TODO: substitute with `extern void test();`
+}
+
+void initTest() {
+    struct pcb_t* process = allocPcb();
+
+    process->p_s.pc = (memaddr) test;
+    process->p_s.sp = RAM_TOP - FRAME_SIZE;
+    process->p_s.cpsr = STATUS_ALL_INT_ENABLE(process->p_s.cpsr) | STATUS_SYS_MODE;
+    process->p_s.CP15_Control = CP15_DISABLE_VM(process->p_s.CP15_Control);
+
+    insertProcQ(&nucleus.readyQueue, process);
+    nucleus.currentProcess++;
+}
+
 int main() {
     // Populate the four new areas in the ROM reserved frame
     initHandler(INT_NEWAREA, int_handler);
@@ -48,6 +65,9 @@ int main() {
 
     // Initialize all nucleus maintained semaphores
     initSemaphores();
+
+    // Instantiate test process
+    initTest();
 
     return 0;
 }
