aaaf5349f71253b9bf12b01741433c422281a3b2
Enrico Martelli <mrcnrt95e30@gmail.com>
2016-05-16 19:44:11 +0200

    Added scheduler implementation

--- phase2/src/scheduler.c
+++ phase2/src/scheduler.c
@@ -1,5 +1,60 @@
+#include <libuarm.h>
+
+#include "clist.h"
+#include "const.h"
+
 #include "scheduler.h"
+#include "initial.h"
+
+unsigned int sliceTime = 0;
+unsigned int clockTime = 0;
+unsigned int processTime = 0;
+
+void tick() {
+    unsigned int time = getTODLO();
+    int deltaSlice = SCHED_TIME_SLICE + sliceTime - time;
+    int deltaClock = SCHED_PSEUDO_CLOCK + clockTime - time;
+
+    if (deltaSlice <= 0) {
+        sliceTime = time;
+        deltaSlice = SCHED_TIME_SLICE;
+    }
+
+    if (deltaClock <= 0) {
+        clockTime = time;
+        deltaClock = SCHED_PSEUDO_CLOCK;
+    }
+
+    if (deltaClock <= deltaSlice) {
+        setTIMER(deltaClock);
+    } else {
+        setTIMER(deltaSlice);
+    }
+}
+
+void deadlock() {
+    PANIC();
+}
 
 void scheduler() {
+    tick();
 
+    if (nucleus.currentProcess == NULL) {
+        if (clist_empty(nucleus.readyQueue)) {
+            if (nucleus.processCount == 0) {
+                HALT();
+            } else if (nucleus.softBlockCount == 0) {
+                deadlock();
+            } else {
+                setSTATUS(STATUS_ALL_INT_ENABLE(getSTATUS()));
+                WAIT();
+            }
+        }
+
+        nucleus.currentProcess = removeProcQ(&nucleus.readyQueue);
+    }
+
+    processTime = getTODLO();
+
+    LDST(&nucleus.currentProcess->p_s);
 }
--- phase2/src/scheduler.h
+++ phase2/src/scheduler.h
@@ -1,3 +1,7 @@
 #pragma once
 
+extern unsigned int sliceTime;
+extern unsigned int clockTime;
+extern unsigned int processTime;
+
 void scheduler();
